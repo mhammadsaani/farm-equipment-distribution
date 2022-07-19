de1d9e6d8835614355b4a2515e3200889df6d55c
6hislain <ghislainmike80@gmail.com>
2022-07-19 16:00:50 +0200

    started qna

--- qna/urls.py
+++ qna/urls.py
@@ -3,4 +3,16 @@ from django.views.generic import TemplateView
 from . import views
 
 app_name = "qna"
-urlpatterns = []
+urlpatterns = [
+    path("question", views.question_index, name="question.index"),
+    path("question/create", views.question_create, name="question.create"),
+    path("question/search", views.question_search, name="question.search"),
+    path("question/<int:id>/edit", views.question_edit, name="question.edit"),
+    path("question/<int:id>/delete", views.question_delete, name="question.delete"),
+    path("question/<slug:slug>", views.question_show, name="question.show"),
+    path("answer", views.answer_index, name="answer.index"),
+    path("answer/create", views.answer_create, name="answer.create"),
+    path("answer/<int:id>/edit", views.answer_edit, name="answer.edit"),
+    path("answer/<int:id>/delete", views.answer_delete, name="answer.delete"),
+    path("answer/<int:id>", views.answer_show, name="answer.show"),
+]
--- qna/views.py
+++ qna/views.py
@@ -1,3 +1,87 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth.decorators import login_required
+from django.utils.text import slugify
+from django.contrib import messages
+from .models import Question, Answer
+from after_sale_service.models import Tag
+
 
 # Create your views here.
+@require_http_methods(["GET"])
+def question_index(request):
+    tags = Tag.objects.order_by("-id")[:10]
+    questions = Question.objects.order_by("-id")[:10]
+    return render(
+        request, "qna/question/index.html", {"tags": tags, "questions": questions}
+    )
+
+
+# TODO: validate tags
+@login_required(login_url="signin")
+@require_http_methods(["GET", "POST"])
+def question_create(request):
+    if request.method == "GET":
+        tags = Tag.objects.order_by("-id")[:10]
+        return render(request, "qna/question/create.html", {"tags": tags})
+
+    elif request.method == "POST":
+        title = request.POST["title"]
+        content = request.POST["content"]
+        category = request.POST["category"]
+
+        if Tag.objects.filter(name=category).exists():
+            tag = Tag.objects.get(name=category)
+        else:
+            tag = Tag(name=category, slug=slugify(category), user_id=request.user.id)
+            tag.save()
+
+        question = Question(
+            title=title,
+            slug=slugify(title),
+            content=content,
+            category_id=tag.id,
+            user_id=request.user.id,
+        )
+        question.save()
+        messages.info(request, "Question saved")
+        return redirect("qna:question.index")
+
+
+def question_search(request):
+    pass
+
+
+def question_edit(request, id):
+    pass
+
+
+def question_delete(request, id):
+    question = get_object_or_404(Question, pk=id)
+    question.delete()
+    messages.info(request, "Question deleted")
+    return redirect("qna:question.index")
+
+
+def question_show(request, slug):
+    pass
+
+
+def answer_index(request):
+    pass
+
+
+def answer_create(request):
+    pass
+
+
+def answer_edit(request, id):
+    pass
+
+
+def answer_delete(request, id):
+    pass
+
+
+def answer_show(request, slug):
+    pass
